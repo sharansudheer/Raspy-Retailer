ee4ef69a5f88b82f30e415b3f6e07efa4f6e14fe
sharansudheer <sharan.sharansudheer@gmail.com>
2026-01-19 15:16:10 +0530

    feat0:rebase
    rebase and restart

--- RaspyRetailer/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace RaspyRetailer.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
